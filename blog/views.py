65e7e248de360a544361f1973666007cf8e8a977
alejandro <alegutierrezmarin@gmail.com>
2026-01-30 20:08:32 -0400

    recomendar post por email

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 def post_list(request):
@@ -36,13 +37,32 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
     # recibe un id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    sent = False
+
     if request.method == "POST":
-        # formulario enviado
+        # los campos del formulario que pasaron la validacion
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # el formulario pasa la validacion
             cd = form.cleaned_data
-            # email enviado
-        else:
-            form = EmailPostForm()
-        return render(request, "blog/post/share.html", {"post": post, "form": form})
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{cd['name']} ({cd['email']}) " f"recommends you read {post.title}"
+            )
+            message = (
+                f"Lee el post de mi blog: {post.title} \nHaz clic asi: {post_url}\n\n"
+                f"Te lo envia tu amigo: {cd['name']}, con el siguiente mensaje: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd["to"]],
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
